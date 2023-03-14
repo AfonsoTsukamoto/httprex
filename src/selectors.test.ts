f15d328ab2c3be3650f6918cd7d5f244219a44a7
AfonsoTsukamoto <afonsotsukamoto@gmail.com>
2023-03-15 01:50:36 +0900

    Fix selector tests

--- src/selectors.test.ts
+++ src/selectors.test.ts
@@ -1,14 +1,41 @@
-import { assert, expect, test } from 'vitest';
+import { vi, afterEach, expect, test } from 'vitest';
 import { findCodeBlocks  } from './selectors';
 import { Host } from './host';
-import { withTestPage, TestPage } from './test/dom/loader';
+import { getTestDOM, TestPage } from './test/dom/loader';
+
+afterEach(async () => {
+  vi.unstubAllGlobals()
+});
 
 describe('find code blocks', () => {
-  test('finds code in github gist', () => {
-    withTestPage({ page: TestPage.SIMPLE_GIST }, () => {
-      expect(findCodeBlocks(Host.GITHUB)).toBe({});
-    });
+  test('finds code in github gist', async () => {
+    const jsdom = await getTestDOM(TestPage.SIMPLE_GIST);
+    vi.stubGlobal('document', jsdom.window.document);
+    const found: HTMLDivElement = findCodeBlocks(Host.GITHUB)?.item(0) as HTMLDivElement;
+
+    expect(found.outerHTML).toBe(
+      '<div class="highlight highlight-source-httpspec" dir="auto"><pre><span class="pl-k">POST</span>'+
+      '<span class="pl-c1"> https://google.com</span>\n' +
+      '<span class="pl-s"><span class="pl-v">content-type:</span> application/json</span>\n' +
+      '\n' +
+      '{\n' +
+      '    <span class="pl-ii">email: foo@bar.com</span>\n' +
+      '}</pre></div>');
   });
-  test('finds code in gitlab snippet', () => {
+
+  test('finds code in gitlab snippet', async () => {
+    const jsdom = await getTestDOM(TestPage.SIMPLE_SNIPPET);
+    vi.stubGlobal('document', jsdom.window.document);
+    const found: HTMLDivElement = findCodeBlocks(Host.GITLAB)?.item(0) as HTMLDivElement;
+    expect(found.outerHTML).toBe(
+      '<pre class="code highlight js-syntax-highlight language-http white" lang="http" ' +
+      'data-sourcepos="12:1-19:3" id="code-4"><code><span lang="http" class="line" id="LC1">' +
+      '<span class="err">POST https://google.com</span></span>\n' +
+      '<span lang="http" class="line" id="LC2"><span class="err">content-type: application/json</span></span>\n' +
+      '<span lang="http" class="line" id="LC3"></span>\n' +
+      '<span lang="http" class="line" id="LC4"><span class="err">{</span></span>\n' +
+      '<span lang="http" class="line" id="LC5"><span class="err">    email: foo@bar.com</span></span>\n' +
+      '<span lang="http" class="line" id="LC6"><span class="err">}</span></span></code></pre>'
+    );
   });
 });
--- src/test/dom/loader.ts
+++ src/test/dom/loader.ts
@@ -1,5 +1,6 @@
-import * as fs from 'fs';
 import { JSDOM, ConstructorOptions } from 'jsdom'
+
+import fs from 'fs';
 
 export enum TestPage {
   SIMPLE_GIST = 'simple_gist',
@@ -8,22 +9,7 @@ export enum TestPage {
 
 export const PAGE_FILE: Record<TestPage, string> = {
   [TestPage.SIMPLE_GIST]: 'src/test/dom/data/simple-gist.html',
-  [TestPage.SIMPLE_SNIPPET]: 'src/test/dom/data/simple-gist.html'
+  [TestPage.SIMPLE_SNIPPET]: 'src/test/dom/data/simple-snippet.html'
 }
 
-
-export const withTestPage = (
-  { page, opts }: {page: TestPage, opts?: ConstructorOptions },
-  testFn: (dom?: JSDOM) => any
-) => {
-  const pageFile = PAGE_FILE[page];
-  console.log({ pageFile });
-  if (pageFile) {
-    JSDOM.fromFile(pageFile, opts).then((dom: JSDOM) => {
-      const doc = global.document;
-      global.document = dom.window.document;
-      testFn(dom)
-      global.document = doc;
-    });
-  }
-}
+export const getTestDOM = async (page: TestPage): Promise<JSDOM> => await JSDOM.fromFile(PAGE_FILE[page]);
